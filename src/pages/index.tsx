6eb61c37a99dfcc1711adb354c686e8c94ea4051
Honza Penicka <honza.penicka@gmail.com>
2024-06-14 16:00:30 +0200

    center mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,11 @@ const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex-grow flex flex-col items-center justify-center pt-16">
-        <h1 className="text-4xl font-bold">Welcome to the OICT App</h1>
+      <div className="flex-grow flex flex-col items-center justify-center pt-16 text-center">
+        <h1 className="text-4xl font-bold">
+          Welcome to the <br className="md:hidden" />
+          OICT App
+        </h1>
       </div>
       <Footer />
     </div>
